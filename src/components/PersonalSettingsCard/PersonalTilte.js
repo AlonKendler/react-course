d3901a2271c74ab4edb623f52c40f66f3b769bfa
AlonKendler <alonkendler@gmail.com>
2019-10-07 16:37:34 +0300

    added grid to personalForm

--- src/components/PersonalSettingsCard/PersonalTilte.js
+++ src/components/PersonalSettingsCard/PersonalTilte.js
@@ -1,17 +1,15 @@
 import React from "react";
+import messages from "./messages";
 
 //TODO : instead of writing plain text in the components
-// create messages.js file for each component and set it's messages there
+// create messages.js file for each component and set it's messages there.
 
 //TODO do not use divs for text components, use TYPOGRAPHY
 function PersonalTitle() {
   return (
     <div className="titleHeader">
-      <div className="titleBold">הגדרות אישיות</div>
-      <div className="titleReg">
-        ניתן לעדכן את הגדרות החשבון שלך. שים לב, עדכון הכתובת שלך יעדכן את
-        ההדפות החיפוש שלך באופן אוטומטי.
-      </div>
+      <div className="titleBold">{messages.personalSettingsTitle}</div>
+      <div className="titleReg">{messages.personalSettingsDescription}</div>
     </div>
   );
 }
