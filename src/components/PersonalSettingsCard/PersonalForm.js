59e2bc22260d650a00a6a8881475b6c61b9adf27
AlonKendler <alonkendler@gmail.com>
2019-10-07 19:20:44 +0300

    PassText component for  PersonalForm component

--- src/components/PersonalSettingsCard/PersonalForm.js
+++ src/components/PersonalSettingsCard/PersonalForm.js
@@ -1,29 +1,28 @@
 import React from "react";
+import messages from "./messages";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import messages from "./messages";
+import Grid from "@material-ui/core/Grid";
+import PasswordText from "./PasswordText";
 
 const useStyles = makeStyles(theme => ({
   container: {
-    display: "flex",
-    flexWrap: "wrap",
     boxShadow: "0 0px 2px 0 rgba(0, 0, 0, 0.35)",
     borderRadius: "3px"
   },
   textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 200
+    width: "100%"
   },
-  dense: {
-    marginTop: 19
+  textFieldWrap: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2)
   },
-  menu: {
-    width: 200
+  icon: {
+    color: "rgb(47, 114, 133)"
   }
 }));
 
-export default function PersonalForm() {
+function PersonalForm() {
   const classes = useStyles();
   //TODO read about use state and hooks before you write this code
   const [values, setValues] = React.useState({
@@ -40,73 +39,80 @@ export default function PersonalForm() {
     setValues({ ...values, [name]: event.target.value });
   };
 
-  //   let streetClasses = ["textFieldStreet"];
-
   //TODO add material ui GRID
   // for desktop - the form should be divided as in the picture
   // for mobile - one field per "row"
 
-  // TODO add the mising "reset password" text
-  // TODO learn about material ui icons and how to add them
-  // TODO if you dont have a good reason - dont add unnessesary attirubutes like id
   return (
     <form className={classes.container} noValidate autoComplete="off">
-      <TextField
-        id="standard-name"
-        label="שם"
-        className={classes.textField}
-        value={values.name}
-        onChange={handleChange("name")}
-        margin="normal"
-      />
-      <TextField
-        id="standard-name"
-        label="טלפון"
-        className={classes.textField}
-        value={values.phone}
-        onChange={handleChange("phone")}
-        margin="normal"
-      />
-      <TextField
-        id="standard-name"
-        label="אמייל"
-        className={classes.textField}
-        value={values.email}
-        onChange={handleChange("email")}
-        margin="normal"
-      />
-      <TextField
-        id="standard-password-input"
-        label="סיסמה"
-        className={classes.textField}
-        type="password"
-        autoComplete="current-password"
-        margin="normal"
-      />
-      <TextField
-        id="standard-name"
-        label="רחוב"
-        className={classes.textField}
-        value={values.streetName}
-        onChange={handleChange("streetName")}
-        margin="normal"
-      />
-      <TextField
-        id="standard-name"
-        label="מספר"
-        className="textFieldStreet"
-        value={values.streetNum}
-        onChange={handleChange("streetNum")}
-        margin="normal"
-      />
-      <TextField
-        id="standard-name"
-        label={messages.city}
-        className="textFieldStreet"
-        value={values.city}
-        onChange={handleChange("city")}
-        margin="normal"
-      />
+      <Grid container>
+        <Grid item className={classes.textFieldWrap} xs={12} sm={6}>
+          <TextField
+            label={messages.name}
+            className={classes.textField}
+            value={values.name}
+            onChange={handleChange("name")}
+            margin="normal"
+          />
+        </Grid>
+        <Grid item className={classes.textFieldWrap} xs={12} sm={6}>
+          <TextField
+            label={messages.phone}
+            className={classes.textField}
+            value={values.phone}
+            onChange={handleChange("phone")}
+            margin="normal"
+          />
+        </Grid>
+        <Grid item className={classes.textFieldWrap} xs={12} sm={6}>
+          <TextField
+            label={messages.email}
+            className={classes.textField}
+            value={values.email}
+            onChange={handleChange("email")}
+            margin="normal"
+          />
+        </Grid>
+        <Grid item className={classes.textFieldWrap} xs={12} sm={6}>
+          <TextField
+            label={messages.password}
+            className={classes.textField}
+            type="password"
+            autoComplete="current-password"
+            margin="normal"
+          />
+          <PasswordText></PasswordText>
+        </Grid>
+        <Grid item className={classes.textFieldWrap} xs={6} sm={4}>
+          <TextField
+            label={messages.street}
+            className={classes.textField}
+            value={values.streetName}
+            onChange={handleChange("streetName")}
+            margin="normal"
+          />
+        </Grid>
+        <Grid item className={classes.textFieldWrap} xs={6} sm={4}>
+          <TextField
+            label={messages.houseNumber}
+            className={classes.textField}
+            value={values.streetNum}
+            onChange={handleChange("streetNum")}
+            margin="normal"
+          />
+        </Grid>
+        <Grid item className={classes.textFieldWrap} xs={12} sm={4}>
+          <TextField
+            label={messages.city}
+            className={classes.textField}
+            value={values.city}
+            onChange={handleChange("city")}
+            margin="normal"
+          />
+        </Grid>
+      </Grid>
     </form>
   );
 }
+
+export default PersonalForm;
