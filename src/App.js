7f644dd48c2a639d25428dbb858261fb3b366d4b
AlonKendler <alonkendler@gmail.com>
2019-10-10 18:41:58 +0300

    re-order dir of SettingsCard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
 import "./App.css";
-import NotificationSettingsCard from "./components/NotificationSettingsCard";
-import PersonalSettingsCard from "./components/PersonalSettingsCard";
-import notificationMessages from "./components/NotificationSettingsCard/messages";
-import personalMessages from "./components/PersonalSettingsCard/messages";
+import NotificationSettingsCard from "./components/SettingsCards/NotificationSettingsCard";
+import PersonalSettingsCard from "./components/SettingsCards/PersonalSettingsCard";
 
 // TODO  (read about container components vs components )
-
+// TODO: do not use <div> if there is no need
+// TODO: create a SettingsPage container that will contain all of the component's logic
 function App() {
+  // TODO: crate data object with settings and pass them to the components
+  // TODO: add functionallity that when you press "אני לא רוצה לקבל שום הודעות"
+  // TODO: it switches off all of the notification
+
+  // const settingsData = {
+
+  // };
+
+  // THEORITICALLY
+  // 1-> get the settings data from the server
+  // 2-> display them on the settigns card
+  // 3-> user changes data as he wishes
+  // 4-> when user presses save it saves the data
+
   return (
-    <div>
-      <NotificationSettingsCard data={notificationMessages} />
-      <PersonalSettingsCard data={personalMessages} />
-    </div>
+    <>
+      <NotificationSettingsCard />
+      <PersonalSettingsCard />
+    </>
   );
 }
 
